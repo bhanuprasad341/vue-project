5a3694f47885a60295a144d6574d23a9bda6be05
bhanuprasad341 <96613931+bhanuprasad341@users.noreply.github.com>
2022-02-04 00:33:50 +0530

    condition

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -14,10 +14,55 @@
       <router-link to="/form10">Form 10</router-link>
       <router-link to="/task1">TASK 1</router-link>
       <router-link to="/task2">TASK 2</router-link>
+      <router-link to="/task3">TASK 3</router-link>
+       <ol>
+         <li v-for="(author,index) in users" :key="index">
+           <label><b>Author :</b></label>{{author.author}}<br><label><b>Country :</b></label>{{author.country}}<br><label><b>ImageLink :</b></label>{{author.imageLink}}
+           <label><b>Language :</b></label>{{author.language}}<br><label><b>Link :</b></label>{{author.link}}<br><label><b>Pages :</b></label>{{author.pages}}<br><label><b>Title :</b></label>{{author.title}}
+           <br><label><b>Year :</b></label>{{author.year}}
+         </li>
+       </ol><br>
+       <ol>
+         <li v-for="(title,index,) in movies" :key="index">
+          <div v-if="title['Major Genre']=='Drama'"> 
+            <label><b>Title :</b></label>{{title.Title}}<br>
+            <label><b>US Gross :</b></label>{{title["US Gross"]}}<br>
+            <label><b>Worldwide Gross :</b></label>{{title["Worldwide Gross"]}}<br>
+           <label><b>US DVD Sales :</b></label>{{title["US DCD Sales"]}}<br>
+           <label><b>Production Budget :</b></label>{{title["Production Budget"]}}<br>
+           <label><b>Release Date :</b></label>{{title["Release Date"]}}<br>
+           <label><b>MPAA Rating :</b></label>{{title["MPAA Rating"]}}
+           <br><label><b>Running Time min :</b></label>{{title["Running Time min"]}}<br>
+           <label><b>Distributor :</b></label>{{title.Distributor}}<br>
+           <label><b>Source :</b></label>{{title.Source}}<br>
+           <label><b>Major Genre :</b></label>{{title["Major Genre"]}}<br>
+           <label><b>Type :</b></label>{{title.Type}}<br>
+           <label><b>Director :</b></label>{{title.Director}}<br>
+           <label><b>Rotten Tomatoes Rating:</b></label>{{title["Rotten Tomatoes Rating"]}}<br>
+           <label><b>IMDB Rating :</b></label>{{title["IMDB Rating"]}}<br>
+           <label><b>IMDB Votes :</b></label>{{title["IMDB Votes"]}}
+           </div>
+         </li>
+       </ol>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import usersData from "./users.json";
+import moviedata from "./movies.json";
+
+export default {
+  data() {
+    return {
+      users: usersData,
+      movies: moviedata,
+
+    };
+  },
+};
+</script>
 
 <style>
 #app {
